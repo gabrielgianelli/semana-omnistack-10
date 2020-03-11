ff3a0c84f082ed4ce7547975a0ce3b44f2747122
Gabriel Gianelli <gabrielgianelli@gmail.com>
2020-03-10 23:38:05 -0300

    Implementação do frontend web

---
+++ web/src/App-Contador.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+// Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
+// Propriedade: Informações que um componente PAI passa para o componente FILHO
+// Estado: Informações mantidas pelo componente (lembrar: imutabilidade)
+
+import Header from './Header';
+
+
+
+function App() {
+  const  [counter, setCounter] = useState(0);
+  
+  function incrementCounter() {
+    setCounter(counter + 1);
+  }
+  
+  return (
+    <>
+      <h1>Contador: {counter}</h1>
+      <button onClick={incrementCounter}>Incrementar</button>
+    </>
+  );
+}
+
+export default App;
